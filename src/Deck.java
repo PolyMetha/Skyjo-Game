c5262b03ca832aef8d5a619c3fee565caa08dffb
PolyMetha <93182128+PolyMetha@users.noreply.github.com>
2023-04-07 17:45:07 +0200

    Cartes modifiées avec UV

--- src/Deck.java
+++ src/Deck.java
@@ -7,23 +7,7 @@ public class Deck {
     private ArrayList<Card> cards;
     private ArrayList<Card> discardPile;
 
-    private HashMap<Integer, String> dict = new HashMap<>(Map.of(
-        -2,"PC20",
-        -1, "LE09" ,
-        0, "MBE1",
-        1, "IF2B",
-        2, "MT2A",
-        3, "IF3B",
-        4, "LE08",
-        5, "PS2A",
-        6, "LE03",
-        7, "IF1A",
-        8, "GE00",
-        9, "MT3E",
-        10, "PS28",
-        11, "MT28",
-        12, "PS25"
-    ));
+    private HashMap<Integer, String> dict = new HashMap<>();
 
     public Deck(){
         dict.put(-2, "PC20");
@@ -32,30 +16,30 @@ public class Deck {
         dict.put(1, "IF2B");
         dict.put(2, "MT2A");
         dict.put(3, "IF3B");
-        dict.put(4, "PC20");
-        dict.put(5, "LE09");
-        dict.put(6, "PC20");
-        dict.put(7, "LE09");
-        dict.put(8, "PC20");
-        dict.put(9, "LE09");
-        dict.put(10, "PC20");
-        dict.put(11, "LE09");
-        dict.put(12, "PC20");
+        dict.put(4, "LE08");
+        dict.put(5, "PS2A");
+        dict.put(6, "LE03");
+        dict.put(7, "IF1A");
+        dict.put(8, "GE00");
+        dict.put(9, "MT3E");
+        dict.put(10, "PS28");
+        dict.put(11, "MT28");
+        dict.put(12, "PS25");
         
 
         cards = new ArrayList<Card>();
         //adding 5 -2 cards to the deck
         for(int i = 0; i<5; i++){
-            cards.add(new Card(-2, "PC20"));
+            cards.add(new Card(-2, dict.get(-2)));
         }
         //adding 5 0 cards to the deck, 10 will be added in the next loop to have 15 of it
         for(int i = 0; i<5; i++){
-            cards.add(new Card(0, "MBE1"));
+            cards.add(new Card(0, dict.get(0)));
         }
         //adding 10 1->10 cards to the deck
         for(int i = -1; i<=12; i++){
             for(int j=0; j<10; j++){
-                cards.add(new Card(i));
+                cards.add(new Card(i, dict.get(i)));
             }
         }
         Shuffle();
@@ -64,7 +48,7 @@ public class Deck {
     //prints the deck
     public void PrintDeck(){
         System.out.println("\n__________________");
-        cards.forEach((n) -> System.out.print(n.GetValue() + " | "));
+        cards.forEach((n) -> System.out.print(n.GetName() + "("+ n.GetValue() +")"+ " | "));
         System.out.println("\n__________________");
     }
 
