2f086e56eb6c984fb3cb13450b67d6e58272eef2
PolyMetha <93182128+PolyMetha@users.noreply.github.com>
2023-04-19 12:02:12 +0200

    GameLoop Réparée

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -19,6 +19,8 @@ public class GameLoop {
                 i++;
                 play = !its.next().verifyWin();
             }
+            //reinitialise l'itérateur
+            its = players.iterator();
         }
     }
 
