cadb5533875c7ecae998768f9b6d19d852bb3fb5
paulcmt <paulcleent@gmail.com>
2023-05-05 11:50:24 +0200

    Update Utility.java

--- src/Utility.java
+++ src/Utility.java
@@ -5,7 +5,7 @@ public class Utility {
 
     // This function is used to control the input of an integer from the user,
     // ensuring that the integer is within the specified range.
-    static short controlInt(short borne1, short borne2, String message1, String message2)
+    public static short controlInt(short borne1, short borne2, String message1, String message2)
     {
         short indice = 0;
         while (indice < borne1 || indice > borne2) {
@@ -30,7 +30,7 @@ public class Utility {
     }
 
     // This function calculates the score of a player based on the values of their cards.
-    static void calculScore(Player player)
+    public static void calculScore(Player player)
     {
         short score = 0;
         for (Card card : player.getHand()) {
@@ -40,7 +40,7 @@ public class Utility {
     }
 
     // This function displays the scores of all the players in the game.
-    static void displayScore(ArrayList<Player> players)
+    public static void displayScore(ArrayList<Player> players)
     {
         for (Player player : players) {
             calculScore(player); // calculates the score of each player
