74d55837b07f4fd42dd6c39181d2469b26a3fc09
paulcmt <paulcleent@gmail.com>
2023-05-14 21:13:26 +0200

    Update

--- src/CardImgResized.java
+++ src/CardImgResized.java
@@ -6,8 +6,8 @@ import javax.swing.ImageIcon;
 public class CardImgResized extends ImageIcon{
 
     //screen dimensions
-    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    static int screenHeight = (int) screenSize.getHeight();
+    private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private static int screenHeight = (int) screenSize.getHeight();
 
     //the size we want the cards to be
     public static int IMG_HEIGHT = ((screenHeight/2-60)-(20+3*5))/4, IMG_WIDTH = (int)(IMG_HEIGHT*0.778);
